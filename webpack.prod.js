c08097fcb98be94c53e6857bf202acf8ccd86c7d
Jack Bogart <73082872+JackBogart@users.noreply.github.com>
2024-07-14 15:21:31 -0400

    Initial commit

---
+++ webpack.prod.js
@@ -0,0 +1,33 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "production",
+  devtool: "source-map",
+  plugins: [
+    new MiniCssExtractPlugin({filename: "[name].[contenthash].css",}),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimizer: ['...', new CssMinimizerPlugin()],
+  },
+});
