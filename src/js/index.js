a17840b6706c38cb73269a3409034b902a93a945
Jack Bogart <jackbogart0822@gmail.com>
2024-07-15 20:59:26 -0400

    Add search form for to select city
    Users will be able to search up a city/location of their choice to
    retrieve weather data. Weather data is then fetched from VisualCrossing,
    before being parsed as JSON. The JSON is processed to extract the data
    we will display to users regarding weather data.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
-console.log('Hello world!');
+import '../css/style.css';
+import createForecast from './forecast';
+
+const locationForm = document.querySelector('form');
+const location = document.querySelector('#location');
+
+locationForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location.value}\
+    /next7days?unitGroup=us&elements=datetime%2CresolvedAddress%2Ctempmax%2Ctempmin%2Ctemp%2Cprecipprob%\
+    2Cconditions%2Cdescription%2Cicon&include=fcst%2Cdays%2Ccurrent&key=8KTRXTELZJ2FLALH7ANX7K72L&contentType=json`,
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      forecast = createForecast(data);
+      console.log(forecast.dailyWeather);
+    });
+});
+let forecast = null;
+
+console.log(forecast);
